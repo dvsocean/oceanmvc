f0e2b947dfcd97d48141b9c73bcbe98db6b75ac0
Ocean <dvsocean@icloud.com>
2018-07-20 12:13:38 -0700

    deploy without xml files

---
+++ src/main/java/com/ocean/oceanmvc/OceanmvcApplication.java
@@ -0,0 +1,12 @@
+package com.ocean.oceanmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanmvcApplication.class, args);
+	}
+}
